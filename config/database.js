3e9926cbf904ab05c769557607108129d02ec198
luisedr98 <luisdominguez818@gmail.com>
2023-01-24 08:54:36 -0500

    add: config database

--- config/database.js
+++ config/database.js
@@ -13,15 +13,15 @@ const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB
     host : process.env.DB_HOST,
     port : process.env.DB_PORT,
     dialect : process.env.DB_DRIVER,
-    // define : {
-    //     timestamps : false,
-    // },
-    // pool :{
-    //     max : 5,
-    //     min : 0,
-    //     acquire : 30000,
-    //     idle : 1000
-    // },
+    define : {
+        timestamps : false,
+    },
+    pool :{
+        max : 5,
+        min : 0,
+        acquire : 30000,
+        idle : 1000
+    },
 });
 
 export default db;
