a229a8e04a68500f73f9fe6625bc18b9c49ba36b
luisedr98 <luisdominguez818@gmail.com>
2023-01-24 08:58:09 -0500

    fix database config

--- config/database.js
+++ config/database.js
@@ -17,10 +17,11 @@ const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB
         timestamps : false,
     },
     pool :{
-        max : 5,
-        min : 0,
-        acquire : 30000,
-        idle : 1000
+        max: 100,
+        min: 0,
+        acquire: 1000000,
+        idle: 100000,
+        evict: 2000,
     },
 });
 
